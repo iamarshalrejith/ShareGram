731bb379cba7100b82c4e5930b802ca490bc8779
Arshal Rejith <arshalrejithcodes@gmail.com>
2026-08-24 23:04:11 +0530

    Add post creation endpoint

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from app.schemas import PostCreate
 
 app = FastAPI()
 
@@ -26,3 +27,9 @@ def get_post(id:int):
     if id not in text_posts:
         raise HTTPException(status_code=404,detail="Post not found")
     return text_posts.get(id)
+
+@app.post("/posts")
+def create_post(post: PostCreate):
+    new_post = {"title": post.title, "content":post.content}
+    text_posts[max(text_posts.keys()) + 1] = new_post
+    return new_post
