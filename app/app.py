30b179f1f5be2dd60c74ba713cad040c8affc70f
Arshal Rejith <arshalrejithcodes@gmail.com>
2026-08-26 22:04:30 +0530

    Add file upload endpoint

--- app/app.py
+++ app/app.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, File, UploadFile, Form, Depends
 from app.schemas import PostCreate, PostResponse
 from app.db import Post, create_db_and_tables, get_async_session
 from sqlalchemy.ext.asyncio import AsyncSession
 from contextlib import asynccontextmanager
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -10,3 +11,22 @@ async def lifespan(app: FastAPI):
     yield
 
 app = FastAPI(lifespan=lifespan)
+
+@app.post("/upload")
+async def upload_file(
+    file: UploadFile = File(...),
+    caption: str = Form(""),
+    session: AsyncSession = Depends(get_async_session)
+):
+    post = Post(
+        caption = caption,
+        url = "dummy url",
+        file_type="photo",
+        file_name="dummy name",
+    )
+    session.add(post)
+    await session.commit()
+    await session.refresh(post)
+    return post
+
+
