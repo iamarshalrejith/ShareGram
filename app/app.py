9b31369b53df59d16cda19fdd133c25f9362a39d
Arshal Rejith <arshalrejithcodes@gmail.com>
2026-08-28 22:03:42 +0530

    Add feed endpoint

--- app/app.py
+++ app/app.py
@@ -3,6 +3,7 @@ from app.schemas import PostCreate, PostResponse
 from app.db import Post, create_db_and_tables, get_async_session
 from sqlalchemy.ext.asyncio import AsyncSession
 from contextlib import asynccontextmanager
+from sqlalchemy import select
 
 
 @asynccontextmanager
@@ -30,3 +31,25 @@ async def upload_file(
     return post
 
 
+@app.get("/feed")
+async def get_feed(
+    session: AsyncSession = Depends(get_async_session)
+):
+    result = await session.execute(select(Post).order_by(Post.created_at.desc()))
+    posts = [row[0] for row in result.all()]
+    
+    posts_data = []
+    for post in posts:
+        posts_data.append(
+            {
+                "id": str(post.id),
+                "caption": post.caption,
+                "url": post.url,
+                "file_type": post.file_type,
+                "file_name": post.file_name,
+                "created_at": post.created_at.isoformat()        
+            }
+        )
+    return {
+            "posts": posts_data
+        }
