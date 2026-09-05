ee2b69b414ed443160a9b484f17206b649706c16
Arshal Rejith <arshalrejithcodes@gmail.com>
2026-09-05 21:52:55 +0530

    Add user authentication

--- app/app.py
+++ app/app.py
@@ -1,15 +1,17 @@
 from fastapi import FastAPI, HTTPException, File, UploadFile, Form, Depends
-from app.schemas import PostResponse
+from app.schemas import PostResponse, PostCreate, UserCreate, UserRead, UserUpdate
 from app.db import Post, create_db_and_tables, get_async_session
 from sqlalchemy.ext.asyncio import AsyncSession
 from contextlib import asynccontextmanager
 from sqlalchemy import select
 from app.images import imagekit
+from app.users import auth_backend, current_active_user, fastapi_users
 
 import shutil
 import os
 import uuid
 import tempfile
+
 
 
 @asynccontextmanager
@@ -19,7 +21,11 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
-
+app.include_router(fastapi_users.get_auth_router(auth_backend),prefix='/auth/jwt',tags=["auth"])
+app.include_router(fastapi_users.get_register_router(UserRead,UserCreate), prefix="/auth", tags=["auth"])
+app.include_router(fastapi_users.get_reset_password_router(), prefix="/auth",tags=["auth"])
+app.include_router(fastapi_users.get_verify_router(UserRead), prefix="/auth", tags=["auth"])
+app.include_router(fastapi_users.get_users_router(UserRead,UserUpdate), prefix="/users", tags=["users"])
 
 @app.post("/upload")
 async def upload_file(
